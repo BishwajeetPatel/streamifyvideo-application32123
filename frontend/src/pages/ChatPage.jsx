01b87fe0ea6cc970e5ea6bbaac0e177301aadce6
BishwajeetPatel <153221241+BishwajeetPatel@users.noreply.github.com>
2025-08-13 17:00:47 +0530

    Update ChatPage.jsx

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
 import { useQuery } from "@tanstack/react-query";
 import { getStreamToken } from "../lib/api";
-
+ 
 import {
   Channel,
   ChannelHeader,
