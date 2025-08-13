dc304a85d9d80ec72730bbc357f6bcb7365cd9cf
BishwajeetPatel <153221241+BishwajeetPatel@users.noreply.github.com>
2025-08-13 16:58:57 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../lib/api";
 
-const useLogin = () => {
+const useLogin = () => { 
   const queryClient = useQueryClient();
   const { mutate, isPending, error } = useMutation({
     mutationFn: login,
