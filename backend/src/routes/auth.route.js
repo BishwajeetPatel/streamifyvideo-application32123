13a960d616139d114560d83ec748ae9fe86f2b41
BishwajeetPatel <153221241+BishwajeetPatel@users.noreply.github.com>
2025-08-13 17:08:05 +0530

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-
+ 
 const router = express.Router();
 
 router.post("/signup", signup);
