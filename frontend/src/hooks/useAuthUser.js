ce2e14ff07e168bc516b76b8e7b5da42514ae944
BishwajeetPatel <153221241+BishwajeetPatel@users.noreply.github.com>
2025-08-13 16:13:05 +0530

    Update useAuthUser.js

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -5,7 +5,7 @@ const useAuthUser = () => {
   const authUser = useQuery({
     queryKey: ["authUser"],
     queryFn: getAuthUser,
-    retry: false, // auth check
+    retry: false, // auth check poit
   });
 
   return { isLoading: authUser.isLoading, authUser: authUser.data?.user };
